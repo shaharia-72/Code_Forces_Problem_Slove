51dc2814cf1f72e31d38f23436ac626822ca80ad
Shaharia <shahariahossain72@gmail.com>
2023-04-18 15:56:59 +0600

    cpp

---
+++ Problem_sloved_with_C++-program/B_Basic_Data_Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float e;
+    double f;
+
+    cin >> a >> b >> c >> e >> f;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << e << endl;
+    cout << f << endl;
+    return 0;
+}
